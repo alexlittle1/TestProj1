98ddb6d885ee8a05d820f3625f6ac9bbc6eb2072
alexlittle1 <95433252+alexlittle1@users.noreply.github.com>
2021-12-02 08:43:55 -0800

    Initial commit

---
+++ src/testproj1/TestProj1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testproj1;
+
+/**
+ *
+ * @author 800000001
+ */
+public class TestProj1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("works");
+    }
+    
+}
